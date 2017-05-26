35cb28b0ba8df82b77ccaf2b58b43e30ff6959bd
Karina Kuroda <Karina Kuroda>
2017-05-26 09:07:12 -0300

    first commit

---
+++ CompileCshtmlWithDotNetCore/CompileCshtmlWithDotNetCore/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompileCshtmlWithDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        private readonly ViewRender view;
+        public ValuesController(ViewRender view)
+        {
+
+            this.view = view;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+
+              var teste = new Teste();
+            teste.Code = 123;
+            var html = this.view.Render("Index",teste);
+            return new string[] { "value1", html };
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CompileCshtmlWithDotNetCore/CompileCshtmlWithDotNetCore/ViewRender.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+
+
+namespace CompileCshtmlWithDotNetCore
+{
+    public class Teste
+    {
+        public int Code { get; set; }
+    }
+    public class HomeController : Controller
+    {
+
+        private readonly ViewRender view;
+
+        public HomeController(ViewRender view)
+        {
+            this.view = view;
+        }
+
+        public string Test()
+        {
+            // render ~/Views/Emails/ResetCode
+            var teste = new Teste();
+            teste.Code = 123;
+            var html = this.view.Render("Views/Index",teste);
+
+            return "";
+
+        }
+    }
+
+
+
+    public class ViewRender
+    {
+        private IRazorViewEngine _viewEngine;
+        private ITempDataProvider _tempDataProvider;
+        private IServiceProvider _serviceProvider;
+
+        public ViewRender(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            _viewEngine = viewEngine;
+            _tempDataProvider = tempDataProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+        public string Render<TModel>(string name, TModel model)
+        {
+            var actionContext = GetActionContext();
+
+            var viewEngineResult = _viewEngine.FindView(actionContext, name, false);
+
+            if (!viewEngineResult.Success)
+            {
+                throw new InvalidOperationException(string.Format("Couldn't find view '{0}'", name));
+            }
+
+            var view = viewEngineResult.View;
+
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary())
+                    {
+                        Model = model
+                    },
+                    new TempDataDictionary(
+                        actionContext.HttpContext,
+                        _tempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                view.RenderAsync(viewContext).GetAwaiter().GetResult();
+
+                return output.ToString();
+            }
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.RequestServices = _serviceProvider;
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
+    }
+
+
+}
